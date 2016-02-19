ee179bf57b9af26abf0802e5512bd5613baeeb31
pellu <jordanpelluard@gmail.com>
2016-02-19 17:20:14 +0100

    Ajout FOS

---
+++ app/cache/prod/twig/16/16d58459a3a324e5a9f6f9e4d0384529ffb3b01f6534f1ceaa0c0c4e17df84ae.php
@@ -0,0 +1,34 @@
+<?php
+
+/* UserBundle:Default:index.html.twig */
+class __TwigTemplate_a4dbbae8af4b7e7a947bc0fdeeeae44c5867039cdea27bffdd44a5c1d601a384 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello World!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "UserBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* Hello World!*/
+/* */
