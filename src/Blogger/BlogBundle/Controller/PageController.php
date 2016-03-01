a9dafb49e8fa916147b55a1a7b4338086930a284
pellu <jordanpelluard@gmail.com>
2016-03-01 13:23:26 +0100

    debug page blog

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -12,7 +12,7 @@ class PageController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()
-            ->getEntityManager();
+            ->getManager();
 
         $blogs = $em->createQueryBuilder()
             ->select('b')
